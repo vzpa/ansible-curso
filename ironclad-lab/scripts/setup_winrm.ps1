4fba9d4c480eaae7870321022dad6a42bfeabdbf
Jean Paul <104743589+vzpa@users.noreply.github.com>
2026-06-07 13:17:30 -0500

    Delete ironclad-lab directory

--- ironclad-lab/scripts/setup_winrm.ps1
+++
@@ -1,8 +0,0 @@
-# Este script verifica el cumplimiento post-remediación
-$firewall = Get-NetFirewallProfile | Where-Object {$_.Enabled -eq 'True'}
-if ($firewall.Count -eq 3) {
-    Write-Output "COMPLIANCE: PASSED"
-} else {
-    Write-Error "COMPLIANCE: FAILED"
-    exit 1
-}
